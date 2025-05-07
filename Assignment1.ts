1f1fb7d51249a3f79b1df61cf77f17b5150da5e6
ParthibBr <bparthib77@gmail.com>
2025-05-07 23:39:48 +0600

    6 task done

---
+++ Assignment1.ts
@@ -0,0 +1,120 @@
+{
+  const formatString = (input: string, toUpper?: boolean): string => {
+    if (toUpper === undefined) {
+      return input.toUpperCase();
+    } else if (toUpper) {
+      return input.toUpperCase();
+    } else {
+      return input.toLowerCase();
+    }
+  };
+
+  const filterByRating = (
+    items: { title: string; rating: number }[]
+  ): {
+    title: string;
+    rating: number;
+  }[] => {
+    return items.filter((item) => item.rating >= 4);
+  };
+
+  /*
+  Problem 3:
+  Description: Create a generic function that concatenates multiple arrays of the same type using rest parameters.
+
+  Function Signature:
+
+  function concatenateArrays<T>(...arrays: T[][]): T[]
+  Example:
+
+  concatenateArrays(["a", "b"], ["c"]);       // Output: ["a", "b", "c"]
+  concatenateArrays([1, 2], [3, 4], [5]);     // Output: [1, 2, 3, 4, 5]
+  */
+
+  // const concatenateArrays = <T>(...arrays: T[][]): T[] => {
+  //   return arrays;
+  // };
+
+  // console.log(concatenateArrays(["a", "b"], ["c"]));
+  // console.log(concatenateArrays([1, 2], [3, 4], [5]));
+
+  // #######################
+
+  // #######################
+
+  class Vehicle {
+    public make: string;
+    public year: number;
+
+    constructor(make: string, year: number) {
+      this.make = make;
+      this.year = year;
+    }
+
+    getInfo() {
+      console.log(`Make: ${this.make}, Year: ${this.year}`);
+    }
+  }
+
+  class Car extends Vehicle {
+    private model: string;
+
+    constructor(make: string, year: number, model: string) {
+      super(make, year);
+      this.model = model;
+    }
+
+    getModel() {
+      console.log(`Model: ${this.model}`);
+    }
+  }
+
+  const processValue = (value: string | number): number => {
+    if (typeof value === "string") {
+      return value.length;
+    } else {
+      return value * 2;
+    }
+  };
+
+  interface Product {
+    name: String;
+    price: number;
+  }
+
+  const getMostExpensiveProduct = (products: Product[]): Product | null => {
+    let max = products[0];
+
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].price > max.price) {
+        max = products[i];
+      }
+    }
+
+    return max ?? null;
+  };
+
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+  }
+
+  const getDayType = (day: Day): string => {
+    if (day === Day.Sunday) {
+      return "Weekend";
+    }
+
+    return "Weekday";
+  };
+
+  /*
+  
+  */
+
+
+}
